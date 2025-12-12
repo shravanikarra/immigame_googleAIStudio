5a749f661cd232dbc7076564232a6d9031becf60
Shravani Karra <52153356+shravanikarra@users.noreply.github.com>
2025-12-11 20:38:31 -0600

    Can locally run now, fixed nom issues

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,12 @@ export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, '.', '');
+  const apiKey = env.GEMINI_API_KEY || env.API_KEY || '';
   return {
     plugins: [react()],
     define: {
       // This allows 'process.env.API_KEY' to work in client-side code
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+      'process.env.API_KEY': JSON.stringify(apiKey)
     }
   };
 });
